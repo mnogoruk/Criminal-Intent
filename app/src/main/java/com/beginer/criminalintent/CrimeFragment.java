2c1d77624170bf0cf75422d115b9566049cea60f
daniil <lionless072@gmail.com>
2020-04-29 16:16:48 +0300

    В диалоговое окно с календарём передайтся дата из фрагмета с преступлением

--- app/src/main/java/com/beginer/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/beginer/criminalintent/CrimeFragment.java
@@ -79,7 +79,7 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 FragmentManager fragmentManager = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
                 if (fragmentManager != null) {
                     dialog.show(fragmentManager, DIALOG_DATE);
                 }
