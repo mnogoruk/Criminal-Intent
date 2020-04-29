429282899fbf3315e672aa029501585b05c3de9f
daniil <lionless072@gmail.com>
2020-04-29 16:01:24 +0300

    диалоговое окно с календао=рём

--- app/src/main/java/com/beginer/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/beginer/criminalintent/DatePickerFragment.java
@@ -3,6 +3,8 @@ package com.beginer.criminalintent;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +15,11 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity()).setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null).create();
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .setView(v)
+                .create();
     }
 }
